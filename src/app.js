768697bfce3b01d7547c68d30421eca1c31afbde
JayPatel <jay.patel1@gameskraft.com>
2024-05-01 15:32:55 +0530

    code moved from index to app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// middlewares
+app.cors(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Import Routers
+
+// Define API for specific Route
+
+export { app };
