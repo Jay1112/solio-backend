e11b757585d70ad56a6d2dcb753f2e367e3dab8d
JayPatel <jay.patel1@gameskraft.com>
2024-05-28 17:06:08 +0530

    user login route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,30 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 import { User } from '../models/user.model.js';
 import { generateOTP, getOTPExpiry } from '../utils/utilityFunctions.js';
 
+// generate Refresh And Access Token
+const generateAccessAndRefreshToken = async (userId) => {
+   try {
+     const user = await User.findById(userId);
+     if(user){
+        const accessToken  = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken ;
+
+        await user.save({ validateBeforeSave : false });
+
+        return {
+            refreshToken,
+            accessToken
+        }
+     }else{
+        return null;
+     }
+   } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating access and refresh token...");
+   }
+}
+
 // Register new User in Database
 const registerUser = asyncHandler( async ( req, res) => {
     // extract details : username, email, password
@@ -167,10 +191,83 @@ const userVerification = asyncHandler( async (req, res) => {
     }
 });
 
+// Login User
+const loginUser = asyncHandler( async ( req, res ) => {
+    // extract details from body
+    const { email, password } = req.body ;
+
+    if(!email || !password){
+        throw new ApiError(400,"email and password are required to login");
+    }
+
+    // fetch user details from database
+    const user = await User.findOne({ email });
+    if(!user){
+        throw new ApiError(400,"User does not exist!");
+    }
+
+    // check whether the password is correct or not
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Wrong Password");
+    }
+
+    // User is verified or not
+    const isVerified = user.verified ;
+
+    if(!isVerified){
+        // generate OTP
+        const otp = generateOTP();
+        const otpExpiry = getOTPExpiry();
+
+        // send OTP on email
+        /* TO DO */
+
+        const updatedUser = await User.findByIdAndUpdate(
+            user._id,
+            {
+                $set : {
+                    otp : otp,
+                    otpExpiry : otpExpiry,
+                }
+            },
+            {
+                new  : true
+            }
+        ).select(
+            "-password -refreshToken -otp -otpExpiry"
+        );
+
+        res.status(200).json(
+            new ApiResponse(200,updatedUser,"OTP has been sent Successfully!!")
+        );
+    }else{
+        const { refreshToken, accessToken } = await generateAccessAndRefreshToken(user._id);
+
+        const loggedInUser = await User.findById(user._id).select(
+            "-password -refreshToken -otp -otpExpiry"
+        );
+
+        const options = {
+            httpOnly : true,
+            secure : true
+        }
+
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+            new ApiResponse(200, loggedInUser,"User LoggedIn Successfully!")
+        );
+    }
+});
+
 
 
 export {
     registerUser,
     userVerification,
-    regenerateOTP
+    regenerateOTP,
+    loginUser
 }
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { registerUser, userVerification, regenerateOTP } from '../controllers/auth.controller.js';
+import { registerUser, userVerification, regenerateOTP, loginUser } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.route("/register").post(registerUser);
 authRouter.route("/verify-otp").post(userVerification);
 authRouter.route("/regenerate-otp").post(regenerateOTP);
+authRouter.route("/login").post(loginUser);
 
 export default authRouter;
