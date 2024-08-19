62b526f34f1cc8c71e8b83d3cbc741a9a4b0d450
Jay Patel <jay.patel1@gameskraft.com>
2024-08-19 11:56:55 +0530

    personalized APIs

--- src/controllers/personalized.controller.js
+++ src/controllers/personalized.controller.js
@@ -46,7 +46,8 @@ const createUserPersonalized = asyncHandler( async ( req, res) => {
 
     const newPersonlizedObj = await Personalized.create({
         type,
-        customData : personalizedData
+        customData : personalizedData,
+        user : req.user?._id
     });
 
     if(!newPersonlizedObj){
@@ -60,7 +61,86 @@ const createUserPersonalized = asyncHandler( async ( req, res) => {
     );
 });
 
+const updateUserPersonalized = asyncHandler( async ( req, res) => {
+    const { personalizedId ,type, personalizedData } = req.body ;
+
+    // check all required fields are present or not
+    const isAnyEmptyField = [personalizedId, type, personalizedData].some((field) => {
+        return !field;
+    });
+    if(isAnyEmptyField){
+        return res
+        .status(400)
+        .json(
+            new ApiError(400,"All Fields are required!")
+        )
+    }
+
+    const isValidFormat = personalizedObj.isValidObject(type, personalizedData);
+
+    if(!isValidFormat){
+        return res
+            .status(400)
+            .json(
+                new ApiError(400,"Invalid Body Data Format!")
+            )
+    }
+
+    const updatedPersonalized = await Personalized.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set : {
+                type,
+                customData
+            }
+        },
+        {
+            new  : true
+        }
+    );
+
+    if(!updatedPersonalized){
+        return res.status(400).json(
+            new ApiResponse(400,{}, "Error Occurring while modifying personalized item")
+        );
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,{}, "Personalized Item Modified Successfully!")
+    );
+});
+
+const deleteUserPersonalized = asyncHandler( async ( req, res) => {
+    const { personalizedId } = req.body ;
+
+    // check all required fields are present or not
+    const isAnyEmptyField = [personalizedId].some((field) => {
+        return !field;
+    });
+    if(isAnyEmptyField){
+        return res
+        .status(400)
+        .json(
+            new ApiError(400,"All Fields are required!")
+        )
+    }
+
+    const deletedPersonalized = await Personalized.deleteOne({ _id : personalizedId });
+
+    if(!deletedPersonalized){
+        return res.status(400).json(
+            new ApiResponse(400,{}, "Error Occurring while creating personalized item")
+        );
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,{}, "Personalized Item Removed Successfully!")
+    );
+});
+
 export {
     getUserPersonalizedItems,
-    createUserPersonalized
+    createUserPersonalized,
+    updateUserPersonalized,
+    deleteUserPersonalized
 };
--- src/routers/personalized.router.js
+++ src/routers/personalized.router.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { 
     getUserPersonalizedItems,
-    createUserPersonalized
+    createUserPersonalized,
+    updateUserPersonalized,
+    deleteUserPersonalized
 } from '../controllers/personalized.controller.js';
 import { verifySession } from '../middlewares/auth.middleware.js';
 
@@ -10,5 +12,7 @@ const personalizedRouter = express.Router();
 // secure routes
 personalizedRouter.route("/all").get(verifySession,getUserPersonalizedItems);
 personalizedRouter.route("/create").post(verifySession,createUserPersonalized);
+personalizedRouter.route("/update").post(verifySession,updateUserPersonalized);
+personalizedRouter.route("/delete").post(verifySession,deleteUserPersonalized);
 
 export default personalizedRouter;
