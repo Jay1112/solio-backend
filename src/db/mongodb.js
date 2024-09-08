2b740017b4d4ecb60ef676ace6b8cf18bf144f62
Jay Patel <jay.patel1@gameskraft.com>
2024-09-08 22:01:17 +0530

    console removed

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -5,7 +5,7 @@ export async function connectToDB(){
     try{
         const url = `${process.env.MONGODB_URL}/${DB_NAME}`;
         const connectionObj = await mongoose.connect(url);
-        console.log(`MongoDB Connected Successfully! DB Host : ${connectionObj.connection.host}`);
+        // console.log(`MongoDB Connected Successfully! DB Host : ${connectionObj.connection.host}`);
     }catch(error){
         console.log("MongoDB Connection Failed...");
         process.exit(1);
